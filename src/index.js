266050e5f2d77ab2a3a3be71ed863c3a8f37402d
tychkovas <tychkovas@yandex.ru>
2021-04-29 14:19:24 +0400

    fix(pageload): теперь возращается promise

--- src/index.js
+++ src/index.js
@@ -8,17 +8,18 @@ const getNameFile = (url) => url.replace(/^\w*?:\/\//mi, '') // ^https?:\/\/
 
 const pageLoad = (pageAddress, outputPath) => {
   const nameSaveFile = getNameFile(pageAddress);
-  console.log('nameSaveFile: ', nameSaveFile);
+  // console.log('nameSaveFile: ', nameSaveFile);
   const pathSaveFile = path.join(outputPath, nameSaveFile);
-  console.log(' - pathSaveFile = ', pathSaveFile);
+  // console.log(' - pathSaveFile = ', pathSaveFile);
 
-  axios.get(pageAddress)
+  const result = axios.get(pageAddress)
     .then((response) => response.data)
     .catch((err) => console.log('\n error axios get: err.response.status =', err.response.status))
     .then((data) => fsp.writeFile(pathSaveFile, data))
+    .then(() => pathSaveFile)
     .catch((error) => console.log('\n error write file =', error));
 
-  return pathSaveFile;
+  return result;
 };
 
 export default pageLoad;
