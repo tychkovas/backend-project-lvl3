ab94f1040ae5551b9b822740013ffb4152372638
tychkovas <tychkovas@yandex.ru>
2021-10-06 15:50:02 +0400

    chore(parser): clean code

--- src/transform.js
+++ src/transform.js
@@ -5,10 +5,6 @@ import path from 'path';
 
 const getNameLoadFile = (prefix, link) => `${prefix}${link.replace(/\//mig, '-')}`;
 
-// https: //ru.hexlet.io/courses
-// "/assets/professions/nodejs.png"
-// "ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-nodejs.png"
-
 const typeAssets = [
   { selector: 'link', attr: 'href' },
   { selector: 'img', attr: 'src' },
@@ -22,7 +18,7 @@ const getPageForSave = (data, pathSave, url) => {
 
   const $ = cheerio.load(data);
 
-  const findAssets = (item) => { // console.log('p', item.selector, item.attr);
+  const findAssets = (item) => {
     const elements = $(item.selector);
     elements.each((i, el) => {
       const link = $(el).attr(item.attr);
@@ -49,7 +45,7 @@ const getPageForSave = (data, pathSave, url) => {
 
 export default getPageForSave;
 
-// /*
+/*
 const result = getPageForSave(
   // eslint-disable-next-line no-multi-str
   '<!DOCTYPE html> \
@@ -75,23 +71,4 @@ const result = getPageForSave(
   'https://ru.hexlet.io/courses');
 
 console.log('\n\nresult =', result);
-// */
-
-// <!DOCTYPE html>
-// <html lang="ru">
-//   <head>
-//     <meta charset="utf-8">
-//     <title>Курсы по программированию Хекслет</title>
-//     <link rel="stylesheet" media="all" href="https://cdn2.hexlet.io/assets/menu.css">
-//     <link rel="stylesheet" media="all" href="ru-hexlet-io-courses_files/ru-hexlet-io-assets-application.css">
-//     <link href="ru-hexlet-io-courses_files/ru-hexlet-io-courses.html" rel="canonical">
-//   </head>
-//   <body>
-//     <img src="ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-nodejs.png" alt="Иконка профессии Node.js-программист">
-//     <h3>
-//       <a href="/professions/nodejs">Node.js-программист</a>
-//     </h3>
-//     <script src="https://js.stripe.com/v3/"></script>
-//     <script src="ru-hexlet-io-courses_files/ru-hexlet-io-packs-js-runtime.js"></script>
-//   </body>
-// </html>
+*/
