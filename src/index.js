88983beec39ff83a103ba9b990b52e1550155009
tychkovas <tychkovas@yandex.ru>
2021-10-12 12:34:23 +0400

    feat(all): add step 4(rebase)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 /* eslint-disable max-len */
 import path, { join } from 'path';
 import axios from 'axios';
+import 'axios-debug-log';
+import debug from 'debug';
 import fs, { promises as fsp } from 'fs';
 
 import getPageForSave from './parsing.js';
+
+const nameSpaceLog = 'page-loader';
+
+const log = debug(nameSpaceLog);
+
+debug('booting %o', nameSpaceLog);
 
 const getNameFile = (url, separator = '') => url.replace(/^\w*?:\/\//mi, '')
   .replace(/\/$/, '')
@@ -12,19 +20,24 @@ const getNameFile = (url, separator = '') => url.replace(/^\w*?:\/\//mi, '')
 
 const getNameDir = (nameFile) => path.parse(nameFile).name.concat('_files');
 
-const loadFiles = ({ href, path: pathSave }, _outputPath) => axios({
-  method: 'get',
-  url: href,
-  responseType: 'stream',
-})
-  .catch((error) => console.log('\n error axios get status',
-    error.response.status, ', url =', error.config.url))
-  .then((response) => {
-    response.data.pipe(fs.createWriteStream(path.join(_outputPath, pathSave)));
+const loadFiles = ({ href, path: pathSave }, _outputPath) => {
+  log('load file:', href);
+  return axios({
+    method: 'get',
+    url: href,
+    responseType: 'arraybuffer',
   })
-  .catch((error) => console.log('\n error write file =', error));
+    .catch((error) => console.error('error axios get status',
+      error.response.status, ', url =', error.config.url))
+    .then((response) => {
+      log('save file:', href, 'name:', pathSave);
+      return fsp.writeFile(path.join(_outputPath, pathSave), response.data);
+    })
+    .catch((error) => console.error('error write file:', error.message));
+};
 
 const pageLoad = (pageAddress, outputPath) => {
+  log('start load %o', nameSpaceLog);
   const nameSaveFile = getNameFile(pageAddress, '_');
   const pathSaveFile = path.join(outputPath, nameSaveFile);
   const pathSave = getNameDir(getNameFile(pageAddress, '-'));
@@ -32,30 +45,36 @@ const pageLoad = (pageAddress, outputPath) => {
 
   const checkOrCreateOutputPath = (response) => fsp
     .access(outputPath, fs.constants.F_OK)
-    .catch(() => fsp.mkdir(outputPath, { recursive: true }))
+    .catch(() => fsp.mkdir(outputPath, { recursive: true })
+      .then(() => log('directory creation:', outputPath)))
     .then(() => response);
 
+  log('load html:', pageAddress);
+
   return axios.get(pageAddress)
-    .catch((err) => console.log('\n error axios get: err.response.status =',
-      err.response.status))
+    .catch((err) => console.error('error load html, status:',
+      err.response.status, err.message))
     .then(checkOrCreateOutputPath)
     .catch(() => console.error('cannot access output path', outputPath))
     .then((response) => response.data)
     .then((data) => {
       const { html, assets: dataLinks } = getPageForSave(data, pathSave, pageAddress);
+      log('save html:', pathSaveFile);
       fsp.writeFile(pathSaveFile, html, 'utf-8');
       return dataLinks;
     })
-    .catch((error) => console.log('\n error writeFile page =', error))
+    .catch((error) => console.error('error write page:', error.message))
     .then((dataLinks) => {
       if (dataLinks.length > 0) {
+        log('creating a folder:', nameSaveFile);
         fsp.mkdir(pathSaveDir);
       }
       return dataLinks;
     })
-    .catch((error) => console.log('\n error mkdir =', error))
+    .catch((error) => console.error('error mkdir =', error.message))
     .then((dataLinks) => Promise.all(dataLinks.map((item) => loadFiles(item, outputPath))))
-    .catch((error) => console.log('\n error Promise all =', error))
+    .catch((error) => console.error('error loadFiles =', error.message))
+    .then(() => log('finish load %o', nameSpaceLog))
     .then(() => pathSaveFile);
 };
 
