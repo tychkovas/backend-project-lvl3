868a76e6418a59c7b8e9f7af06fe86fe459b3edb
Aleksandr T <tychkovas@yandex.ru>
2021-10-17 00:57:43 +0400

    refactor(main): changed the sequence of promises calls change log message

--- src/index.js
+++ src/index.js
@@ -21,40 +21,41 @@ const getNameFile = (url, separator = '') => url.replace(/^\w*?:\/\//mi, '')
 const getNameDir = (nameFile) => path.parse(nameFile).name.concat('_files');
 
 const loadFiles = ({ href, path: pathSave }, _outputPath) => {
-  log('load file:', href);
+  log('  load file:', href);
   return axios({
     method: 'get',
     url: href,
     responseType: 'arraybuffer',
   })
     .then((response) => {
-      log('save file:', href, 'name:', pathSave);
+      log('  save file:', href, 'as:', pathSave);
       return fsp.writeFile(path.join(_outputPath, pathSave), response.data);
     });
 };
 
 const pageLoad = (pageAddress, outputPath) => {
-  log('\nstart load %o', nameSpaceLog);
-  log('\npageAddress: ', pageAddress);
+  log('---- start load %o ----', nameSpaceLog);
+  log('pageAddress: ', pageAddress);
   log('outputPath:  ', outputPath);
   const nameSaveFile = getNameFile(pageAddress, '_');
-  const pathSaveFile = path.join(outputPath, nameSaveFile);
+  const pathSaveFile = path.join(outputPath, nameSaveFile); //
   const pathSave = getNameDir(getNameFile(pageAddress, '-'));
   const pathSaveDir = join(outputPath, pathSave);
 
   log('load html:', pageAddress);
 
   return axios.get(pageAddress)
+    // .then((response) => );
     .then((response) => {
       const { html, assets: dataLinks } = getPageForSave(response.data, pathSave, pageAddress);
       log('save html:', pathSaveFile);
+      if (dataLinks.length !== 0)log('creating a folder:', nameSaveFile);
       const promiseMkDir = (dataLinks.length === 0) ? null : fsp.mkdir(pathSaveDir);
-      return Promise.all([fsp.writeFile(pathSaveFile, html, 'utf-8'), promiseMkDir])
-      // return fsp.writeFile(pathSaveFile, html, 'utf-8')
-        .then(() => dataLinks);
+      const promiseWriteFile = fsp.writeFile(pathSaveFile, html, 'utf-8');
+      return Promise.all([promiseWriteFile, promiseMkDir]).then(() => dataLinks);
     })
     .then((dataLinks) => Promise.all(dataLinks.map((item) => loadFiles(item, outputPath))))
-    .then(() => log('finish load %o/n', nameSpaceLog))
+    .then(() => log('---- finish load %o ----', nameSpaceLog))
     .then(() => pathSaveFile);
 };
 
